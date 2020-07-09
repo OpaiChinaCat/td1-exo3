f99e7addff524dcce5f84f5483e719f2330ff3a8
Jean-Philippe <68045559+OpaiChinaCat@users.noreply.github.com>
2020-07-09 17:10:37 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     if (nb < 10) {
-        nb += nb + 1
+        nb += 1
         basic.showNumber(nb)
     } else {
-        nb += 1
+        nb = 1
         basic.showNumber(nb)
     }
 })
